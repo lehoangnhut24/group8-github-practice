f990f0c7970514ee2bcce24c23de062a3b8edad1
Phung Phi <phungphi4989@gmail.com>
2026-05-15 09:46:15 +0700

    Sua loi vi tri file

--- student.py
+++
@@ -1,9 +0,0 @@
-def kiem_tra_mssv_trung(ds_sinh_vien, mssv):
-    for sv in ds_sinh_vien:
-        if sv.mssv == mssv:
-            return True
-    return False
-def chuan_hoa_ten(ten):
-    return ' '.join(word.capitalize() for word in ten.strip().split())
- def sap_xep_theo_ten(ds):
-    return sorted(ds, key=lambda x: x.ten)
