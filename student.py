64faaf767615c94e82927aa4e2c58619516c14b4
Phung Phi <phungphi4989@gmail.com>
2026-05-15 09:39:31 +0700

    Add sap xep theo ten

--- student.py
+++ student.py
@@ -5,3 +5,5 @@ def kiem_tra_mssv_trung(ds_sinh_vien, mssv):
     return False
 def chuan_hoa_ten(ten):
     return ' '.join(word.capitalize() for word in ten.strip().split())
+ def sap_xep_theo_ten(ds):
+    return sorted(ds, key=lambda x: x.ten)
