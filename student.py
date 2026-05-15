c49bd25f204c5199f79d3d0acf9b290c7c1400e8
Phung Phi <phungphi4989@gmail.com>
2026-05-15 09:36:08 +0700

    Add chuan hoa ten sinh vien

--- student.py
+++ student.py
@@ -3,3 +3,5 @@ def kiem_tra_mssv_trung(ds_sinh_vien, mssv):
         if sv.mssv == mssv:
             return True
     return False
+def chuan_hoa_ten(ten):
+    return ' '.join(word.capitalize() for word in ten.strip().split())
